0e8f41eeceb5ecfd788b66e17309f3a445960815
tttwwy <tttwwy@126.com>
2014-09-18 20:22:04 +0800

    feature_generate

--- pickup.py
+++ pickup.py
@@ -1,20 +1,47 @@
 __author__ = 'Administrator'
-#coding=utf-8
-label = ("VIDEO","NOVEL","GAME","TRAVEL","VIDEO","LOTTERY","OTHER")
-def pickup(file_name_read,file_name_save):
-    with open(file_name_read,"r") as file_read:
-        with open(file_name_save,"r") as file_write:
-            for line in file_read:
-                line = line.strip()
-                session
-                if line:
-                    label,query,title = line.split("\t")
-                    labels = label.split(" | ")
+# coding=utf-8
+# label = ("VIDEO", "NOVEL", "GAME", "TRAVEL", "VIDEO", "LOTTERY", "OTHER")
+import sys
+
+
+def read_train(file_name_read):
+    with open(file_name_read, "r") as file_read:
+        session = []
+        for line in file_read:
+            line = line.strip()
+            if line:
+                label, query, title = line.split("\t")
+                labels = label.split(" | ")
+                for index,label in enumerate(labels):
+                    labels[index] = label[6:]
+                query = query.split(" ")
+                title = title.split(" ")
+                session.append([labels,query,title])
+            else:
+                yield session
+                session = []
+        yield session
+
+
+def generate_feature(base_file,feature_file):
+    with open(feature_file,"w") as f:
+        for session in read_train(base_file):
+            features = []
+            for index,labels,query,title in enumerate(session):
+                if "UNKNOWN" not in labels and "TEST" not in labels:
+                    for word in query:
+                        features.append(word)
+                    for index in enumerate(query):
+                        features.append("".join(query[index:index+2]))
+                    for word in title:
+                        features.append(word)
+                    for index in enumerate(title):
+                        features.append("".join(title[index:index+2]))
+
                     for label in labels:
-                        label = label[6:]
-                    query = query.split(" ")
-                    title = title.split(" ")
-                    if "UNKNOWN" not in labels and "TEST" not in labels:
-                        for label in labels:
-                            file_write.write("{0} {1}".format(label,))
+                        f.write("{0} {1}\n".format(label," ".join(features)))
+
+
+
+generate_feature(sys.argv[1],sys.argv[2])
 
