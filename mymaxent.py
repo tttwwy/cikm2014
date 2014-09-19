4e7acf5030a4fa008ef189b8a94a0ae82a08db2f
tttwwy <tttwwy@126.com>
2014-09-19 11:55:37 +0800

    logging

--- mymaxent.py
+++ mymaxent.py
@@ -3,15 +3,21 @@ import collections
 import maxent.pymaxent as pymaxent
 import maxent.cmaxent as cmaxent
 import pickup
-
+import logging
+logging.basicConfig(level=logging.DEBUG,
+            format=' %(asctime)s  %(funcName)s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            )
 
 class Maxent():
     def __init__(self):
+
         self.m = cmaxent.MaxentModel()
         self.test_dict = collections.defaultdict(list)
         self.labels = ("VIDEO", "NOVEL", "GAME", "TRAVEL", "VIDEO", "LOTTERY", "OTHER")
 
     def train(self, feature_file):
+        logging.info("training begin")
         self.m.begin_add_event()
         with open(feature_file, "r") as f:
             for line in f:
@@ -22,6 +28,7 @@ class Maxent():
 
         self.m.end_add_event()
         self.m.train(150, 'lbfgs', 4, 1E-05)
+        logging.info("training end")
         self.m.save("data/model.txt")
 
     def predict(self, feature):
@@ -35,11 +42,15 @@ class Maxent():
     def test(self, test_data, test_submit, result):
 
         # 将测试数据上下文读入内存
+        logging.info("reading test data begin")
         for session in pickup.read_train(test_data):
             for labels, query, title in session:
                 if "TEST" in labels:
                     query_str = "".join(query)
                     self.test_dict[query_str].append(session)
+        logging.info("reading test data end")
+
+        logging.info("submit test begin")
 
         with open(test_submit, "r") as f_submit:
             with open(result, "w") as f_result:
@@ -63,4 +74,5 @@ class Maxent():
                                 predict_result = self.predict(features)
                                 return predict_result
 
+        logging.info("submit test end")
 
