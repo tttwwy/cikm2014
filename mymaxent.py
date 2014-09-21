29458835b4fb705805f869a175adf66c3c1e5a2b
tttwwy <tttwwy@126.com>
2014-09-21 09:47:49 +0800

    调参数

--- mymaxent.py
+++ mymaxent.py
@@ -85,12 +85,16 @@ class Maxent():
             predict_results.append(self.session_predict(query_list,session))
 
         logging.info("predict:{0}\nsessions:{1}\n".format(query_list,predict_results))
-        dict = self.test_dict = collections.defaultdict(int)
-        for predict_result in predict_results:
-            dict[" ".join(predict_result[0])] += 1
+        result = sorted(predict_results,key=lambda x:x[1],reverse=True)
+        predict_result = result[0][0]
 
-        dict = sorted(dict.iteritems(),key=lambda x:dict[x],reverse=True)
-        predict_result = dict[0][0].split(" ")
+
+        # dict = self.test_dict = collections.defaultdict(int)
+        # for predict_result in predict_results:
+        #     dict[" ".join(predict_result[0])] += 1
+        # dict = sorted(dict.iteritems(),key=lambda x:dict[x],reverse=True)
+        # predict_result = dict[0][0].split(" ")
+
         logging.debug("predict_result:{0}".format(predict_result))
 
         return predict_result
