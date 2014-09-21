5a1d209d71196606d5436a0f25ece6b2fa5794e9
tttwwy <tttwwy@126.com>
2014-09-22 00:23:18 +0800

    go

--- segment/ngram.py
+++ segment/ngram.py
@@ -1,2 +1,27 @@
 __author__ = 'Administrator'
 # coding=utf-8
+import collections
+
+class NGram():
+
+    def __init__(self):
+        self.window = 4
+        self.edge_window = 1
+        self.frq = collections.defaultdict(int)
+        self.frq_left = collections.defaultdict(collections.defaultdict(int))
+        self.frq_right = collections.defaultdict(collections.defaultdict(int))
+
+    def count(self,sentence):
+        for length in range(self.window):
+            for index,word in enumerate(sentence):
+                cur_word = sentence[index:index + length + 1]
+                self.frq[cur_word] += 1
+                left_word = sentence[index - self.edge_window:index]
+                right_word = sentence[index + 1:index + self.edge_window + 1]
+                if left_word:
+                    self.frq_left[cur_word][left_word] += 1
+                if right_word:
+                    self.frq_right[cur_word][right_word] += 1
+
+    def cal_inner(self,word):
+
